e2c93dc485fce5e20410ec885af1ac7d185e0950
Dimitre <dimitre@users.noreply.github.com>
2019-05-13 19:27:28 -0300

    license

--- src/ofxDmtrUIElements.h
+++ src/ofxDmtrUIElements.h
@@ -1,6 +1,6 @@
 /**********************************************************************************
 
- Copyright (C) 2018 Dimitre Lima (www.dmtr.org)
+ Copyright (C) 2016-2019 Dimitre Lima (dmtr.org)
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
